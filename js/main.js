8580cb336c5be5ffce4016dcff5df005dcf66eac
Masoud <masoudtabatabaei20@gmail.com>
2022-01-20 21:45:39 +0330

    save dom elements in variable on script

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Dom elements
+const time = document.querySelector("#time");
+const greeting = document.querySelector("#greeting");
+const name = document.querySelector("#name");
+const focus = document.querySelector("#focus");
+
